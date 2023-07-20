58fc18caab1af752bf16637bf8696d4e547c0655
bymyself-yoon <120462702+bymyself-yoon@users.noreply.github.com>
2023-07-21 01:18:40 +0900

    Update keplergl_test.py

--- keplergl_test.py
+++ keplergl_test.py
@@ -1,1 +1,8 @@
 import streamlit as st
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
+
+st.write("This is a kepler.gl map in streamlit")
+
+map_1 = KeplerGl()
+keplergl_static(map_1)
